1a1f44439be5967b619e76911dd90267b233806b
reginardzss <84197750+reginardzss@users.noreply.github.com>
2025-02-10 21:23:26 -0600

    correct installations

---
+++ ocelote-portafolio/src/app/components/main-component.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+
+@Component({
+  selector: 'main-component',
+  imports: [NavBarComponent],
+  templateUrl: './main-component.component.html',
+})
+export class MainComponent {
+
+}
