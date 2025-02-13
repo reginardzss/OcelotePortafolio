14eb5196fcee32e4ba2787eb5f86cb767587f3c2
reginardzss <84197750+reginardzss@users.noreply.github.com>
2025-02-13 12:34:47 -0600

    hero updates

--- ocelote-portafolio/src/app/components/hero/hero.component.ts
+++ ocelote-portafolio/src/app/components/hero/hero.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-hero',
+  selector: 'hero',
   imports: [],
   templateUrl: './hero.component.html',
   styleUrl: './hero.component.css'
--- ocelote-portafolio/src/app/components/main-component.component.ts
+++ ocelote-portafolio/src/app/components/main-component.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { HeroComponent } from './hero/hero.component';
 
 @Component({
   selector: 'main-component',
-  imports: [NavBarComponent],
+  imports: [NavBarComponent,HeroComponent],
   templateUrl: './main-component.component.html',
 })
 export class MainComponent {
