e00b086c6cc4d273c2b24534fc075f3292e44de6
reginardzss <84197750+reginardzss@users.noreply.github.com>
2025-03-04 11:25:44 -0600

    admin navbar

--- ocelote/src/app/components/LayoutWrapper.tsx
+++ ocelote/src/app/components/LayoutWrapper.tsx
@@ -2,14 +2,19 @@
 
 import { usePathname } from "next/navigation";
 import Navbar from "./navbar";
+import NavbarAdmin from "./navbarAdmin";
+import path from "path";
 
 export default function LayoutWrapper({ children }: { children: React.ReactNode }) {
   const pathname = usePathname(); // Obtener la ruta actual
 
+  if (pathname === "/pages/admin/login" || pathname === "/pages/admin/register") {
+    return <main>{children}</main>;
+  }
   return (
     <div>
-      {/* Mostrar Navbar solo si NO estamos en /login */}
-      {pathname !== "/pages/admin/login" && pathname !== "/pages/admin/register" && <Navbar />}
+      {/* Mostrar NavbarAdmin si la ruta actual comienza con /pages/admin */}
+      {pathname.startsWith("/pages/admin") ? <NavbarAdmin /> : <Navbar />}
       <main>{children}</main>
     </div>
   );
