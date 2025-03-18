2c4f61c217d169ffb3108701c92314348b9b76f7
reginardzss <84197750+reginardzss@users.noreply.github.com>
2025-03-18 10:32:28 -0600

    navbar admin updated

--- ocelote/src/app/components/LayoutWrapper.tsx
+++ ocelote/src/app/components/LayoutWrapper.tsx
@@ -2,14 +2,20 @@
 
 import { usePathname } from "next/navigation";
 import Navbar from "./navbar";
+import NavbarAdmin from "./admin/navbarAdmin";
 
 export default function LayoutWrapper({ children }: { children: React.ReactNode }) {
   const pathname = usePathname(); // Obtener la ruta actual
 
+  // 🔹 Ocultar navbar en login y register
+  if (pathname.startsWith("/pages/admin/login") || pathname.startsWith("/pages/admin/register")) {
+    return <main>{children}</main>;
+  }
+
   return (
     <div>
-      {/* Mostrar Navbar solo si NO estamos en /login */}
-      {pathname !== "/pages/admin/login" && pathname !== "/pages/admin/register" && <Navbar />}
+      {/* 🔹 Mostrar NavbarAdmin en TODAS las páginas dentro de /pages/admin */}
+      {pathname.startsWith("/pages/admin") ? <NavbarAdmin /> : <Navbar />}
       <main>{children}</main>
     </div>
   );
---
+++ ocelote/src/app/components/admin/navbarAdmin.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import Link from "next/link";
+import Image from "next/image";
+
+const NavbarAdmin: React.FC = () => {
+  return (
+    <nav className="sticky top-0 z-10 bg-black">
+      <div className="flex w-screen h-16 items-center p-8">
+        <Link href="/pages/admin/content/dashboard">
+          <Image
+            src="/logo/ocelote-logo-blanco4x.png"
+            height={30}
+            width={300}
+            alt="Ocelote Logo"
+          />
+        </Link>
+        
+        <div className="w-full flex flex-row-reverse">
+          <ul className="flex flex-row text-white">
+            <li className="px-4">
+                <Link href="/pages/admin/content/upload">UPLOAD</Link>
+            </li>
+            <li className="px-4">
+              <Link href="/pages/projects">PROJECTS</Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default NavbarAdmin;
