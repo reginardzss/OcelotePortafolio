2375ded57ae81715085b7d3ce9693556b054d132
reginardzss <84197750+reginardzss@users.noreply.github.com>
2025-03-31 16:23:20 -0600

    Update hero.tsx

--- ocelote/src/app/components/hero.tsx
+++ ocelote/src/app/components/hero.tsx
@@ -2,23 +2,21 @@ import React from 'react';
 
 const Hero: React.FC = () => {
   return (
-    <section className="w-screen h-screen flex items-center justify-center relative">
-      
-      <video autoPlay loop playsInline className=" w-full h-full object-cover">
-      <source src="/assets/bg-mp.mov" type="video/mp4"></source>
-        Tu navegador no soporta videos en HTML5.
-      </video> 
-      
-      <div className="absolute w-full h-full bg-black opacity-50"></div>
-      <div className="absolute w-full h-full">
-        <div className="w-full h-full flex items-center">
-          <div className="flex flex-col p-16 text-8xl">
+     <section className="w-screen h-screen flex items-center justify-center">
+        <video 
+          autoPlay loop playsInline 
+          className=" w-full h-full object-cover">
+          <source src="/assets/bg-mp.mov" type="video/mp4"/>
+          Your browser does not support the video tag.
+        </video> 
+        <div className="absolute w-screen h-screen bg-black opacity-50"></div>
+        <div className="absolute w-screen h-screen flex items-center">
+        <div className="flex flex-col p-8 text-4xl md:text-6xl lg:text-8xl  ">
             <div className="text-oceloteRed">OCELOTE FILMS</div>
             <div>WHERE CREATIVITY <br /> MEETS FILMMAKING.</div>
           </div>
-        </div>  
-      </div>
-    </section> 
+        </div>
+    </section>  
   );
 };
 
