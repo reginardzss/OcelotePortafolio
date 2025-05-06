356b82d1bc2ce1a4d8027db8c10f374f8760a4fd
reginardzss <84197750+reginardzss@users.noreply.github.com>
2025-05-06 16:24:44 -0600

    Update page.tsx

--- ocelote/src/app/page.tsx
+++ ocelote/src/app/page.tsx
@@ -1,11 +1,13 @@
 "use client"; // Solo si es necesario
 import React from "react";
 import Home from "./pages/home/page";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 
 const App: React.FC = () => {
   return (
     <div className="App">
+      <SpeedInsights/>
       <Home />
     </div>
   );
